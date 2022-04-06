e65592b77cc29de7499b9957f8506da742850cb6
Tommy Long <61881891+Tommy-Long4358@users.noreply.github.com>
2022-04-06 12:19:45 -0700

    Update Writing_Groups.java
    added comments

--- src/main/java/csulb/cecs323/model/Writing_Groups.java
+++ src/main/java/csulb/cecs323/model/Writing_Groups.java
@@ -13,19 +13,29 @@ public class Writing_Groups extends Authoring_Entities {
 
 	@Column
 	private int yearFormed;
-
+	
+	// get method for the head writer of a writing group
 	public String getHeadWriter() {
 		return headWriter;
 	}
-
+	
+	// get method for the year formed
 	public int getYearFormed() {
 		return yearFormed;
 	}
-
+	
+	/*
+	* set method for changing a head writer
+	* @param headWriter - head writer of a group
+	*/
 	public void setHeadWriter(String headWriter) {
 		this.headWriter = headWriter;
 	}
-
+	
+	/*
+	* set method for changing the year formed
+	* @param yearFormed - year formed of the publisher.
+	*/
 	public void setYearFormed(int yearFormed) {
 		this.yearFormed = yearFormed;
 	}
