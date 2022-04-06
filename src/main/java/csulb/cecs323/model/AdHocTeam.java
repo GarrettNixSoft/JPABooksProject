f247de59b2ba2c11976507b990718f7d0b175852
Tommy Long <61881891+Tommy-Long4358@users.noreply.github.com>
2022-04-06 12:05:01 -0700

    Update AdHocTeam.java
    added comments

--- src/main/java/csulb/cecs323/model/AdHocTeam.java
+++ src/main/java/csulb/cecs323/model/AdHocTeam.java
@@ -15,29 +15,38 @@ public class AdHocTeam extends Authoring_Entities {
 			inverseJoinColumns = @JoinColumn(name = "INDIVIDUAL_AUTHORS_EMAIL")
 	)
 	private Set<IndividualAuthor> teamMembers = new HashSet<IndividualAuthor>();
-
+	
+	// Empty Constructor
 	public AdHocTeam() { }
-
+	
+	// Get method for ad hoc team email.
 	public String getAd_hoc_teams_email()
 	{
 		return getEmail();
 	}
-
+	
+	// Set method for ad hoc team email
 	public void setAd_hoc_teams_email(String ad_hoc_teams_email)
 	{
 		setEmail(ad_hoc_teams_email);
 
 	}
-
+	
+	// Get method for teamMembers list.
 	public Set<IndividualAuthor> getTeamMembers() {
 		return teamMembers;
 	}
-
+	
+	// Set method for teamMembers list.
 	public void setTeamMembers(Set<IndividualAuthor> authors)
 	{
 		teamMembers = authors;
 	}
-
+	
+	/*
+	* Adds authors to the teamMembers list and adds the Ad Hoc Team object itself to the author's list.
+	* @param authors an Individual Author object.
+	*/
 	public void addTeamMembers(IndividualAuthor authors)
 	{
 		this.teamMembers.add(authors);
