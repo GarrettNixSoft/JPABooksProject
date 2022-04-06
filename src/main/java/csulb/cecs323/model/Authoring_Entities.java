2786f42ffaf16f8e3fcf6d01c6f22b24f47a0ef9
Tommy Long <61881891+Tommy-Long4358@users.noreply.github.com>
2022-04-06 12:08:30 -0700

    Update Authoring_Entities.java
    added comments

--- src/main/java/csulb/cecs323/model/Authoring_Entities.java
+++ src/main/java/csulb/cecs323/model/Authoring_Entities.java
@@ -52,37 +52,58 @@ public abstract class Authoring_Entities {
 	@OneToMany
 	@JoinColumn(name = "AUTHORING_ENTITY_NAME")
 	private Set<Books> works;
-
+	
+	// Empty Authoring Entity constructor that initializes a name and email.
 	public Authoring_Entities() {
 		this.name = "";
 		this.email = "";
 	}
-
+	
+	/*
+	* Overloaded Authoring Entity constructor that initializes a name and email from the parameter.
+	* @param name name of the Authoring Entity
+	* @param email email of the Authoring Entity
+	*/
 	public Authoring_Entities(String name, String email) {
 		this.name = name;
 		this.email = email;
 	}
-
+	
+	// Get method for the name
 	public String getName() {
 		return name;
 	}
-
+	
+	// Get method for the email.
 	public String getEmail() {
 		return email;
 	}
-
+	
+	// Get method for the work list
 	public Set<Books> getWorks() {
 		return works;
 	}
-
+	
+	/*
+	* Set method that initializes a name with the parameter
+	* @param name  name of the authoring entity.
+	*/
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
+	/*
+	* Set method that initializes an email with the parameter
+	* @param email  email of the authoring entity.
+	*/
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	/*
+	* Set method that initializes a work List with the parameter
+	* @param works  List of books of the authoring entity.
+	*/
 	public void setWorks(Set<Books> works) {
 		this.works = works;
 	}
