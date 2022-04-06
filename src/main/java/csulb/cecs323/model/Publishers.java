4d22a3a100a9b01d7f9632941922ec0dc3efe6c5
Tommy Long <61881891+Tommy-Long4358@users.noreply.github.com>
2022-04-06 12:18:40 -0700

    Update Publishers.java
    added comments

--- src/main/java/csulb/cecs323/model/Publishers.java
+++ src/main/java/csulb/cecs323/model/Publishers.java
@@ -26,40 +26,63 @@ public class Publishers {
 
 	@Column(nullable = false, length = 24, unique = true)
 	private String phone;
-
+	
+	// Empty constructor
 	public Publishers() {}
-
+	
+	/* 
+	* Overloaded constructor to initialize a publisher object
+	* @param name - name of the publisher
+	* @param email - email of the publisher
+	* @param phone - phone number of the publisher
+	*
+	*/
 	public Publishers(String name, String email, String phone) {
 		this.name = name;
 		this.email = email;
 		this.phone = phone;
 	}
-
+	
+	// get method for the name
 	public String getName()
 	{
 		return this.name;
 	}
-
+	
+	/*
+	* set method for changing a publisher name
+	* @param name - name of the publisher
+	*/
 	public void setName(String name)
 	{
 		this.name = name;
 	}
-
+	
+	// get method for the email
 	public String getEmail()
 	{
 		return this.email;
 	}
-
+	
+	/*
+	* set method for changing an email of a publisher 
+	* @param ISBN - book serial number
+	*/
 	public void setEmail(String email)
 	{
 		this.email = email;
 	}
-
+	
+	// get method for the phone
 	public String getPhone()
 	{
 		return this.phone;
 	}
-
+	
+	/*
+	* set method for changing a phone number
+	* @param phone - phone number of the Publisher
+	*/
 	public void setPhone(String phone)
 	{
 		this.phone = phone;
